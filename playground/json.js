58d60665bd0c913efac6871d91ef5b2118fa291e
Felipe Secato <felipe.secato@gmail.com>
2018-09-04 10:53:50 -0300

    working with json

--- playground/json.js
+++ playground/json.js
@@ -2,7 +2,22 @@ let obj = {
     name: 'Felipe'
 }
 
+// obj to string
 let stringObj = JSON.stringify(obj)
 
 console.log(obj)
 console.log(stringObj)
+
+// string (json) to obj
+let personString = '{"name":"Felipe","age": 25}'
+let personObj = JSON.parse(personString)
+
+console.log(personString)
+console.log(personObj)
+console.log("Name: ", personObj.name, " - Age: ", personObj.age)
+console.log('')
+
+console.log('Types:')
+console.log(typeof personString)
+console.log(typeof personObj)
+
